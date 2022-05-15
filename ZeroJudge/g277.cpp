db4cc15db80ae4a66f8209cfa33e1c9e757254ec
HaKkaz(ROG) <luckyanthonyan@gmail.com>
2022-05-16 02:05:59 +0800

    update

--- ZeroJudge/g277.cpp
+++ ZeroJudge/g277.cpp
@@ -1,26 +1,53 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int maxn = 1e6+600;
-long long p[maxn];
-pair<int,int> a[maxn];
-int main(){
-    int n; cin >> n;
-    for(int i=1 ; i<=n ; ++i) 
-        cin >> a[i-1].first , p[i] = p[i-1] + a[i-1].first , a[i-1].second = i;
-    sort(a , a+n);
-    int l = 1 , r = n ;
-    for(int i=0 ; i<n ; ++i){
-        if(r == l) break;
-        if(a[i].second >= l && a[i].second <= r){
-            int pos = a[i].second;
-            int A = p[pos-1] , B = p[n]-p[pos];
-            if( A > B ){
-                r = pos-1;
-            }
-            else{
-                l = pos+1;
-            }
-        }
+#define int long long
+#define fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define _ <<' '<<
+#define ALL(v) v.begin(),v.end()
+#define ft first
+#define sd second
+#define tiii tuple<int,int,int>
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+
+using namespace std;
+const int maxn = 3e5+50;
+int arr[maxn] , psum[maxn];
+vector<int> MN;
+int pos = -1;
+
+void solve(int l , int r){
+    if(l == r){
+        pos = l;
+        return ;
     }
-    cout << p[l]-p[l-1] << '\n';
+
+    while(MN.size() && (MN.back() < l || MN.back() > r)) MN.pop_back();
+    int m = MN.back();
+    MN.pop_back();
+
+    int Rsum = (r >= m ? psum[r] - psum[m] : 0);
+    int Lsum = (m-1 >= l-1 ? psum[m-1] - psum[l-1] : 0);
+
+    if(Lsum > Rsum) solve(l , m-1);
+    else solve(m+1 , r);
+}
+
+signed main(){
+    fast;
+    int n; cin >> n;
+    for(int i=1 ; i<=n ; ++i){
+        cin >> arr[i];
+        psum[i] = psum[i-1] + arr[i];
+        MN.push_back(i);
+    }
+    
+    sort(ALL(MN) , [&](const int &a , const int &b){
+        return arr[a] > arr[b];
+    });
+
+    solve(1,n);
+
+    cout << arr[pos] << '\n';
 }
